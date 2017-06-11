cafa77e3b926568fbe0f1202fd821c8c091518ea
Jean Carlo Cervantes <jcervantes@gbm.net>
2017-06-11 00:08:40 -0600

    Se agrega funcionalidad para obtener el location en tiempo real constantemente y marca los puntos en el mapa.

--- app/src/main/java/com/crmonkistech/seminarioulacit/falcongps/MapsActivity.java
+++ app/src/main/java/com/crmonkistech/seminarioulacit/falcongps/MapsActivity.java
@@ -1,18 +1,81 @@
 package com.crmonkistech.seminarioulacit.falcongps;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.JsonReader;
+import android.util.Log;
+import android.provider.Settings.Secure;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.io.OutputStreamWriter;
+import java.io.BufferedOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.util.JsonWriter;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    private LocationRequest mLocationRequest;
+    /**
+     * Callback for Location events.
+     */
+    private LocationCallback mLocationCallback;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +85,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        createLocationCallback();
+        createLocationRequest();
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Creates a callback for receiving location events.
+     */
+    private void createLocationCallback() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Location location = locationResult.getLastLocation();
+                if(location!=null) {
+                    final Double lat = location.getLatitude();
+                    final Double lon = location.getLongitude();
+                    Log.i("Latitud", lat.toString());
+                    Log.i("Longitud", lon.toString());
+
+                    // Add a marker in Sydney and move the camera
+                    LatLng sydney = new LatLng(lat,lon);
+                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marca en Costa Rica"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
+                    Thread thread = new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            try {
+                                postData(lon, lat);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    thread.start();
+                }
+            }
+        };
     }
 
 
@@ -37,11 +158,93 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (!checkPermissions())
+            requestPermissions();
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback, Looper.myLooper());
+        /*mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Called when a new location is found by the network location provider.
+                            final String lat = Double.toString(location.getLatitude());
+                            final String lon = Double.toString(location.getLongitude());
+                            Log.i("Latitud", lat);
+                            Log.i("Longitud", lon);
+                            Thread thread = new Thread(new Runnable() {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(9.934739, -84.087502);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marca en Costa Rica"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,18));
+                                @Override
+                                public void run() {
+                                    try {
+                                        postData(lon, lat);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                            thread.start();
+                        }
+                    }
+                });*/
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    public void postData(Double la, Double lo) throws JSONException, IOException {
+        URL url = new URL("http://mapas.crmonkistech.com/GuardarLocalizacion.php");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        StringBuilder result = new StringBuilder();
+        try {
+            urlConnection.setDoOutput(true);
+            urlConnection.setChunkedStreamingMode(0);
+            OutputStream out = urlConnection.getOutputStream();
+            OutputStreamWriter outWriter = new OutputStreamWriter(out);
+            JsonWriter writer = new JsonWriter(outWriter);
+            writer.beginObject();
+            writer.name("longitud").value(lo);
+            writer.name("latitud").value(la);
+            writer.name("dispositivo").value(Secure.getString(getContentResolver(), Secure.ANDROID_ID));
+            writer.endObject();
+            writer.close();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            Log.i("respuesta",result.toString());
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
